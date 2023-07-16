c215e3619a0081ca461079cbbf60bd762ce01702
hktaneja <hktaneja2020@gmail.com>
2023-07-15 20:43:25 -0500

    adding play.js file

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '172.19.104.119',// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // Handle incoming data
+  conn.on('data', (data) => {
+  const message = data.toString();
+  console.log('Received:', message);
+  // Additional logic for processing the received data
+});
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();

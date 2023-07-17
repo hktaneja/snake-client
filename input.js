5ea50d328d7ee2fc75487b797e7a7bbca6e3a321
hktaneja <hktaneja2020@gmail.com>
2023-07-17 14:54:19 -0500

    WASD Movement changes

--- input.js
+++ input.js
@@ -13,15 +13,30 @@ const setupInput = (conn) => {
   return stdin;
 };
 
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
 
 // this function check for the ctrl + c input and terminate the game.
 const handleUserInput = (data) => {
-  console.log(data);
   if (data === '\u0003') {
     // Check for Ctrl + C input (ASCII code: \u0003)
     console.log('Terminating the game...');
     process.exit();
   } 
+  if (data === MOVE_UP_KEY) {
+    connection.write('Move: up');
+  }
+  if (data === MOVE_LEFT_KEY) {
+    connection.write('Move: left');
+  }
+  if (data === MOVE_DOWN_KEY) {
+    connection.write('Move: down');
+  }
+  if (data === MOVE_RIGHT_KEY) {
+    connection.write('Move: right');
+  }
 };
 
 
